76e2b1b51ba23abc0d65548cf9f0ee405ca7ab9b
adambsteele <as09am@my.fsu.edu>
2014-09-20 11:19:36 -0400

    Created yahoo weather module w/ simple 'forecast' view

---
+++ lurcat/modules/yahooweather/services.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, render_template, request, flash, url_for, redirect
+import json
+import requests
+
+yahooweatherbp = Blueprint('yahooweatherbp', __name__, url_prefix='/yweather')
+
+class yWeatherObj():
+    forecast = None
+    title = None
+
+
+
+@yahooweatherbp.route('/forecast/')
+def forecast(city='Atlanta', state='ga'):
+    params_dict = {}
+
+    params_dict['format'] = 'json'
+    # city, state
+    city = request.args.get('city')
+    state = request.args.get('state')
+
+    if not city or not state:
+        city = 'Atlanta'
+        state = 'ga'
+
+    params_dict['q'] = ('select * from weather.forecast ' +
+                        'where woeid in (select woeid from geo.places(1) ' + 
+                        'where text="' + city + ', ' + state + '")')
+
+
+    params_dict['env'] = 'store://datatables.org/Falltableswithkeys'
+
+    r = requests.get("https://query.yahooapis.com/v1/public/yql", params=params_dict)
+    jdict = json.loads(r.text)
+
+
+    wxDict = jdict['query']['results']['channel']['item']
+
+    return render_template('yahooweather/forecast.html', city=city, state=state, wxDict=wxDict)
